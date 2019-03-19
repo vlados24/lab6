0f2a3026d16af649ae16594c7c50a982eae8b952
Vladyslav Andrushkevych <andrushkevych2424@gmail.com>
2019-03-19 16:14:56 +0200

    modified lab 5

--- src/test/java/ua/lviv/iot/PhotographerDeviceManagerTest.java
+++ src/test/java/ua/lviv/iot/PhotographerDeviceManagerTest.java
@@ -18,46 +18,46 @@ import main.java.ua.lviv.iot.photographerDevice.model.StudioStand;
 import main.java.ua.lviv.iot.photographerDevice.model.Target;
 
 class PhotographerDeviceManagerTest {
-  
-  List<PhotographerDevice>testDevice = new ArrayList<>();
-  PhotographerDeviceManager testManager = new PhotographerDeviceManager();
-  
-  Camera camera = new Camera(100, 22, Target.LANDSCAPE, "Sony", "Yes", 7, "Intel Core");
-  Lantern lantern = new Lantern(200, 30, Target.CLASSIC_EVENT, 3, "Powerfull");
-  StudioStand studioStand = new StudioStand(17, 25, Target.CLASSIC_EVENT, "Yes", 3, 1);
-  Quadcopter quadcopter = new Quadcopter(5, 8, Target.SPORT_EVENT, 1000, 250, 800);
-  
-@BeforeEach
-void setUp() {
-  testDevice.add(camera);
-  testDevice.add(lantern);
-  testDevice.add(studioStand);
-  testDevice.add(quadcopter);
-  
-}
-  
-    
-  @Test
-  void testSortBySize() {
-      testManager.sortBySize(testDevice, true);
-          Assertions.assertEquals(30, testDevice.get(0).getSize());
-          Assertions.assertEquals(25, testDevice.get(1).getSize());
-          Assertions.assertEquals(22, testDevice.get(2).getSize());
-          Assertions.assertEquals(8, testDevice.get(3).getSize());
-    
-  }
-   
-  
-   @Test
-   void testSortByWeight() {
-          testManager.sortByWeight(testDevice, true);
-          Assertions.assertEquals(200, testDevice.get(0).getWeight());
-          Assertions.assertEquals(100, testDevice.get(1).getWeight());
-          Assertions.assertEquals(17, testDevice.get(2).getWeight());
-          Assertions.assertEquals(5, testDevice.get(3).getWeight());
-          
-  }
 
-  
+	List<PhotographerDevice> testDevice = new ArrayList<>();
+	PhotographerDeviceManager testManager = new PhotographerDeviceManager();
 
+	Camera camera = new Camera(100, 22, Target.LANDSCAPE, "Sony", "Yes", 7, "Intel Core");
+	Lantern lantern = new Lantern(200, 30, Target.CLASSIC_EVENT, 3, "Powerfull");
+	StudioStand studioStand = new StudioStand(17, 25, Target.CLASSIC_EVENT, "Yes", 3, 1);
+	Quadcopter quadcopter = new Quadcopter(5, 8, Target.SPORT_EVENT, 1000, 250, 800);
+
+	@BeforeEach
+	void setUp() {
+		testDevice.add(camera);
+		testDevice.add(lantern);
+		testDevice.add(studioStand);
+		testDevice.add(quadcopter);
+	}
+
+	@Test
+	void testSortBySize() {
+		testManager.sortBySize(testDevice, true);
+		Assertions.assertEquals(30, testDevice.get(0).getSize());
+		Assertions.assertEquals(25, testDevice.get(1).getSize());
+		Assertions.assertEquals(22, testDevice.get(2).getSize());
+		Assertions.assertEquals(8, testDevice.get(3).getSize());
+	}
+
+	@Test
+	void testSortByWeight() {
+		testManager.sortByWeight(testDevice, true);
+		Assertions.assertEquals(200, testDevice.get(0).getWeight());
+		Assertions.assertEquals(100, testDevice.get(1).getWeight());
+		Assertions.assertEquals(17, testDevice.get(2).getWeight());
+		Assertions.assertEquals(5, testDevice.get(3).getWeight());
+
+	}
+	
+	 @Test
+	    void findEssentialDevicesForNatureByTarget() {
+	        List<PhotographerDevice> result = testManager.findEssentialDevicesForNatureByTarget(Target.SPORT_EVENT);
+	        Assertions.assertEquals(Target.SPORT_EVENT, testDevice.get(3).getTarget());
+	      
+	 }
 }
